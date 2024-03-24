0ff9806bbe78de72d8d56bf528fd2d97dade2b39
Wanarak <wanarak.r@gmail.com>
2024-03-24 21:39:15 +0700

    Do assingment ex1,ex2,ex3

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,21 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restuarentName = "Chef's Table by Chef James";
+const TotalPrice = "15000";
+const TotalRestuarent = "1500";
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+TotalPrice = 5000;
+TotalRestuarent =  2500;
+
+console.log(name);
+console.log(lastname);
+console.log(address);
+console.log(TotalPrice);
+console.log(TotalRestuarent);
